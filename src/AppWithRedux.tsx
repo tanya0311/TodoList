3ea52529f84460d92438ae020b2ad96b4e434521
tanya0311 <tanya03.11@mail.ru>
2021-08-22 19:58:49 +0300

    Route, 404, switch

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,51 +1,62 @@
 import {
-  AppBar,
-  Button,
-  Container,
-  IconButton,
-  LinearProgress,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
-import { Menu } from "@material-ui/icons";
-import {  useSelector } from "react-redux";
-import "./App.css";
-import { ErrorSnackbar } from "./components/ErrorSnackbar/ErrorSnackbar";
-import { RequestStatusType } from "./state/app-reducer";
-import { AppRootStateType } from "./state/store";
-import { TodolistsList } from "./components/TodolistsList/TodolistsList";
-
+	AppBar,
+	Button,
+	Container,
+	IconButton,
+	LinearProgress,
+	Toolbar,
+	Typography,
+} from "@material-ui/core"
+import { Menu } from "@material-ui/icons"
+import { useSelector } from "react-redux"
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
+import "./App.css"
+import { ErrorSnackbar } from "./components/ErrorSnackbar/ErrorSnackbar"
+import { RequestStatusType } from "./state/app-reducer"
+import { AppRootStateType } from "./state/store"
+import { TodolistsList } from "./components/TodolistsList/TodolistsList"
+import { Login } from "./features/Login/Login"
 
 type AppType = {
 	demo?: boolean // для storybook
 }
 
-function AppWithRedux({ demo = false }:AppType) {
+function AppWithRedux({ demo = false }: AppType) {
+	const status = useSelector<AppRootStateType, RequestStatusType>(
+		(state) => state.app.status
+	)
 
-  const status = useSelector<AppRootStateType, RequestStatusType>(
-    (state) => state.app.status
-  );
-  
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<ErrorSnackbar />
+				<AppBar position='static'>
+					<Toolbar>
+						<IconButton edge='start' color='inherit' aria-label='menu'>
+							<Menu />
+						</IconButton>
+						<Typography variant='h6'>News</Typography>
+						<Button color='inherit'>Login</Button>
+					</Toolbar>
+					{status === "loading" && <LinearProgress color='secondary' />}
+				</AppBar>
 
-  return (
-    <div className="App">
-      <ErrorSnackbar />
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <Menu />
-          </IconButton>
-          <Typography variant="h6">News</Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-        {status === "loading" && <LinearProgress color="secondary" />}
-      </AppBar>
+				<Container fixed>
+					<Switch>
+						<Route
+							exact
+							path={"/"}
+							render={() => <TodolistsList demo={demo} />}
+						/>
+						<Route path={"/login"} render={() => <Login />} />
 
-      <Container fixed>
-        <TodolistsList demo={demo} />
-      </Container>
-    </div>
-  );
+						<Route path={"/404"} render={() => <h1>404: PAGE NOT FOUND</h1>} />
+						<Redirect from={"*"} to={"/404"} />
+					</Switch>
+				</Container>
+			</div>
+		</BrowserRouter>
+	)
 }
 
-export default AppWithRedux;
+export default AppWithRedux
